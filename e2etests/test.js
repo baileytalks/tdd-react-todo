3bf4c9a51c30bb266b6d516a63ffc564e804ffed
Bailey Kursar <bkursar@gmail.com>
2018-03-06 17:27:26 +0000

    Built out the props for the addtodo component and added passing tests

--- e2etests/test.js
+++ e2etests/test.js
@@ -1,3 +1,4 @@
+/* global describe, it, browser */
 const expect = require('chai').expect;
 
 describe('TodoList App', () => {
@@ -5,6 +6,16 @@ describe('TodoList App', () => {
     browser.url('http://localhost:3000/');
     const actualTitle = browser.getTitle();
 
-    expect(actualTitle).to.eql('Todo List');
+    expect(actualTitle).to.equal('Todo List');
   });
+
+  // it('Should allow me to create a Todo', () => {
+  //   const todoText = "Finish the tech test";
+  //   browser.url('http://localhost:3000/');
+  //   browser.element('.todo-input').setValue(todoText);
+  //   browser.click('.todo-submit');
+  //   const actual = browser.element('.todo-text').getText();
+  //
+  //   expect(actual).to.equal(todoText);
+  // });
 });
---
+++ src/components/addTodo/test.js
@@ -0,0 +1,42 @@
+/* global expect, it, describe, jest, beforeEach */
+
+import React from 'react';
+import Enzyme from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import AddTodo from '.';
+
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('AddTodo component', () => {
+  let component;
+  const submitMock = jest.fn();
+
+  beforeEach(() => {
+    component = shallow(
+      <AddTodo
+        submitTodo={submitMock}
+      />
+    );
+  });
+
+  it('Should render successfully', () => {
+    expect(component.exists()).toEqual(true);
+  });
+
+  it('Should have one input', () => {
+    expect(component.find('.todo-input').length).toEqual(1);
+  });
+
+  it('a submit button should exist', () => {
+    expect(component.find('.todo-submit').length).toEqual(1);
+  });
+
+  it('the button should call the submitTodo function when clicked', () => {
+    component = mount(<AddTodo submitTodo={submitMock} />);
+
+    expect(submitMock.mock.calls.length).toEqual(0);
+    component.find('form').simulate('submit');
+    expect(submitMock.mock.calls.length).toEqual(1);
+  });
+});
