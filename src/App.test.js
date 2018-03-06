3be557c52e7d8b9f91e99d19aa131914610a58bc
Bailey Kursar <bkursar@gmail.com>
2018-03-06 18:43:00 +0000

    Added Redux state and tied it to the props for the input field and the button, so that todos can be submitted

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,23 @@
+/* global it, expect, jest */
+
 import React from 'react';
-import App from './App';
+import { App } from './App';
 import Enzyme from 'enzyme';
 import { shallow } from 'enzyme';
+import { initialState } from './reducers/';
 
 import Adapter from 'enzyme-adapter-react-16';
 Enzyme.configure({ adapter: new Adapter() });
 
 it('App renders without crashing', () => {
-  const component = shallow(<App />);
+  const mockFunction = jest.fn();
+
+  const component = shallow(
+    <App
+      state={initialState}
+      submitTodo={mockFunction}
+    />
+  );
+
   expect(component.exists()).toEqual(true);
 });
